464b602192e19bfae8fe62a55efa2337b242b9c7
Mikhail Shevchenko <mshev@list.ru>
2020-09-04 18:51:12 +0300

    Add priority queue

---
+++ algo-4/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace algo_4.PriorityQueue
+{
+    public class PriorityQueue<T>
+    {
+        private readonly IArray<IArray<T>> _box;
+
+        public int Size { get; private set; }
+
+        public PriorityQueue()
+        {
+            _box = new SingleArray<IArray<T>>();
+        }
+
+
+        public void Enqueue(uint priority, T item)
+        {
+            Size++;
+            if (priority + 1 > _box.Size)
+            {
+                for (var i = _box.Size; i <= priority; i++)
+                {
+                    _box.Append(new FactorArray<T>());
+                }
+            }
+
+            _box.Get((int) priority).Append(item);
+        }
+
+        public T Dequeue()
+        {
+            for (var i = 0; i < _box.Size; i++)
+            {
+                var queue = _box.Get(i);
+                if (queue.Size > 0)
+                {
+                    Size--;
+                    return queue.Remove(0);
+                }
+            }
+
+            throw new Exception("Queue is empty");
+        }
+
+        public bool isEmpty()
+        {
+            return Size == 0;
+        }
+
+        public override string ToString()
+        {
+            if (isEmpty())
+            {
+                return "priority queue is empty";
+            }
+
+            var str = "";
+
+            for (var i = 0; i < _box.Size; i++)
+            {
+                var queue = _box.Get(i);
+                str += "\npriority " + i + ": ";
+
+                if (queue.Size > 0)
+                    for (var j = 0; j < queue.Size; j++)
+                        str += queue.Get(j) + "; ";
+                else
+                    str += "empty";
+            }
+
+            return str;
+        }
+    }
+}
--- algo-4/Program.cs
+++ algo-4/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Diagnostics;
+using algo_4.PriorityQueue;
 
 namespace algo_4
 {
@@ -16,7 +17,83 @@ namespace algo_4
     {
         public static void Main(string[] args)
         {
-            var testItems = new TestItem[]
+            TestPriorityQueue();
+            TestArrays(MakeTestItems());
+        }
+
+        private static void TestPriorityQueue()
+        {
+            var q = new PriorityQueue<int>();
+
+            for (var i = 1; i < 6; i++)
+            {
+                q.Enqueue(0, i);
+            }
+
+            q.Enqueue(1, 6);
+
+            for (var i = 8; i < 13; i++)
+            {
+                q.Enqueue(3, i);
+            }
+
+            const string message = "Priority queue filled: priority '0' has 5 elements, priority '1' has 1 element," +
+                                   "priority '2' is empty, priority '3' has 5 elements:";
+            
+            Console.WriteLine(message);
+            Console.WriteLine(q);
+            
+            Console.WriteLine("\nDequeue element '" + q.Dequeue() + "', priority '0' has 4 elements");
+            Console.WriteLine(q);
+            q.Enqueue(2, 7);
+            
+            Console.WriteLine("\nEnqueue element '7' with priority 2");
+
+            Console.WriteLine(q);
+            Console.WriteLine("\nAll items have been removed from the queue.");
+            var elems = "Retrievers items: ";
+
+            while (!q.isEmpty())
+            {
+                elems += q.Dequeue() + "; ";
+            }
+
+            Console.WriteLine(elems);
+            Console.WriteLine(q);
+        }
+
+        private static void TestArrays(TestItem[] testItems)
+        {
+            var testValues = new[] {100_000, 200_000, 300_000, 1000_000, 2000_000, 3000_000, 10000_000};
+
+
+            foreach (var item in testItems)
+            {
+                var maxTestCount = item.TestAddCount > 0 ? item.TestAddCount : testValues.Length;
+                Console.Write(" \n*** Test: " + item.Name + " ***\n");
+
+                for (var i = 0; i < testValues.Length && i < maxTestCount; i++)
+                {
+                    Console.WriteLine("Test №: " + (i + 1));
+                    Console.WriteLine("Test values: " + testValues[i]);
+                    AddElements(item.Array, testValues[i]);
+                    if (i < item.TestDeleteCount)
+                    {
+                        RemoveElements(item.Array);
+                    }
+                    else
+                    {
+                        item.Array.Clear();
+                    }
+
+                    Console.WriteLine("-----------------");
+                }
+            }
+        }
+
+        private static TestItem[] MakeTestItems()
+        {
+            return new[]
             {
                 new TestItem()
                 {
@@ -69,48 +146,19 @@ namespace algo_4
                     TestDeleteCount = 4,
                 }
             };
-            
-            Test(testItems);
         }
 
-        private static void Test(TestItem[] testItems)
-        {
-            var testValues = new[] {100_000, 200_000, 300_000, 1000_000, 2000_000, 3000_000, 10000_000};
-
-
-            foreach (var item in testItems)
-            {
-                var maxTestCount = item.TestAddCount > 0 ? item.TestAddCount : testValues.Length;
-                Console.Write(" \n*** Test: " + item.Name + " ***\n");
-
-                for (var i = 0; i < testValues.Length && i < maxTestCount; i++)
-                {
-                    Console.WriteLine("Test №: " + (i + 1));
-                    Console.WriteLine("Test values: " + testValues[i]);
-                    AddElements(item.Array, testValues[i]);
-                    if (i < item.TestDeleteCount)
-                    {
-                        RemoveElements(item.Array);
-                    }
-                    else
-                    {
-                        item.Array.Clear();
-                    }
-                    Console.WriteLine("-----------------");
-                }
-            }
-        }
-        
         private static void RemoveElements(IArray<int> array)
         {
             var watch = new Stopwatch();
             watch.Start();
-          
+
             while (array.Size > 0)
             {
                 var index = array.Size / 2;
                 array.Remove(index > 0 ? index : 0);
             }
+
             watch.Stop();
             PrintExecutionTime("Delete", watch);
         }
@@ -130,7 +178,7 @@ namespace algo_4
 
         private static void PrintExecutionTime(string operation, Stopwatch watch)
         {
-            var str = operation +  " time: " + watch.ElapsedMilliseconds;
+            var str = operation + " time: " + watch.ElapsedMilliseconds;
             Console.WriteLine(str);
         }
     }
